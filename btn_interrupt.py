f3ede7420d9e97c19ebd882469f1cbccee27a267
GreasyRapha <ssjishmael@naver.com>
2022-10-26 01:35:30 +0900

    Add files via upload

--- btn_interrupt.py
+++ btn_interrupt.py
@@ -4,9 +4,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 def handler(channel):
-    print("Rising edge detected")
+    print("Clicked")
 
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(24, GPIO.RISING, callback=handler)
+GPIO.add_event_detect(24, GPIO.RISING, callback=handler, bouncetime=300)
 while True:
     time.sleep(1)
--- btn_interrupt2.py
+++ btn_interrupt2.py
@@ -3,11 +3,11 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 def handler(channel):
-    print("Event detected")
+    print("Clicked")
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(24, GPIO.RISING, callback=handler)
+GPIO.add_event_detect(24, GPIO.RISING, callback=handler, bouncetime=300)
 try:
     GPIO.wait_for_edge(23, GPIO.FALLING)
     print("Falling edge detected.")
